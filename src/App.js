f5541983439e9500a1dda42701bb8879fe89d057
renatormc <renatomartinsrmc@gmail.com>
2020-09-12 14:07:21 -0300

    Normal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react'
 import Pictures from './screens/Pictures'
 import PicViewer from './screens/PicViewer'
 import Test from './screens/Test';
+import {prepareFolder} from './services/storage_manager'
 
 import { Router, Scene } from 'react-native-router-flux'
 
 class App extends Component{
+  constructor(props){
+    super(props)
+    prepareFolder()
+  }
+
   render(){
     return (
       <Router>
--- src/services/storage_manager.js
+++ src/services/storage_manager.js
@@ -7,6 +7,7 @@ const PICS_FOLDER = `${RNFU.PicturesDirectoryPath}/sinfpics`
 // const PICS_FOLDER = `/storage/emulated/0/sinfpics`
 
 const getPics = async () => {
+    await prepareFolder()
     let files = await RNFS.readDir(PICS_FOLDER)
     files = files.filter((file) => file.isFile())
     return files.map(file => {
@@ -38,6 +39,7 @@ async function renamePicture(pic, name){
 }
 
 async function savePicture(tempPath, name) {
+    await prepareFolder()
     let newName = name
     let fullName = `${newName}.png`
     let destPath = `${PICS_FOLDER}/${fullName}`
@@ -63,60 +65,60 @@ const prepareFolder = async () => {
     }
 }
 
-const getImageFromCamera = async (callback) => {
+// const getImageFromCamera = async (callback) => {
 
-    if (Platform.OS === 'android') {
+//     if (Platform.OS === 'android') {
 
-        try {
-            await PermissionsAndroid.requestMultiple([
-                PermissionsAndroid.PERMISSIONS.CAMERA,
-                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-            ])
+//         try {
+//             await PermissionsAndroid.requestMultiple([
+//                 PermissionsAndroid.PERMISSIONS.CAMERA,
+//                 PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+//             ])
 
-            const permissionCamera = await PermissionsAndroid.check('android.permission.CAMERA')
-            const permissionWriteStorage = await PermissionsAndroid.check('android.permission.WRITE_EXTERNAL_STORAGE')
+//             const permissionCamera = await PermissionsAndroid.check('android.permission.CAMERA')
+//             const permissionWriteStorage = await PermissionsAndroid.check('android.permission.WRITE_EXTERNAL_STORAGE')
 
 
-            if (!permissionCamera || !permissionWriteStorage) {
-                return {
-                    error: 'Failed to get the required permissions.'
-                }
+//             if (!permissionCamera || !permissionWriteStorage) {
+//                 return {
+//                     error: 'Failed to get the required permissions.'
+//                 }
 
-            }
-        } catch (error) {
-            return {
-                error: 'Failed to get the required permissions.'
-            }
-        }
-    }
-    return true
+//             }
+//         } catch (error) {
+//             return {
+//                 error: 'Failed to get the required permissions.'
+//             }
+//         }
+//     }
+//     return true
 
-}
+// }
 
-async function takePicture(name) {
-    const options = {
-        title: 'Select Avatar',
-        customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
-        storageOptions: {
-            skipBackup: true,
-            path: 'pics'
-        }
-    }
+// async function takePicture(name) {
+//     const options = {
+//         title: 'Select Avatar',
+//         customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+//         storageOptions: {
+//             skipBackup: true,
+//             path: 'pics'
+//         }
+//     }
 
-    ImagePicker.launchCamera(options, async (response) => {
-        if (response.error) {
-            console.log('ImagePicker Error: ', response.error);
-        } else if (response.error) {
-            console.log('ImagePicker Error: ', response.error);
-        } else if (response.customButton) {
-            console.log('User tapped custom button: ', response.customButton);
-        } else {
-            const path = response.path
-            // const info = await savePicture(path, name)
-            return info
-        }
-    })
+//     ImagePicker.launchCamera(options, async (response) => {
+//         if (response.error) {
+//             console.log('ImagePicker Error: ', response.error);
+//         } else if (response.error) {
+//             console.log('ImagePicker Error: ', response.error);
+//         } else if (response.customButton) {
+//             console.log('User tapped custom button: ', response.customButton);
+//         } else {
+//             const path = response.path
+//             // const info = await savePicture(path, name)
+//             return info
+//         }
+//     })
 
-}
+// }
 
-export { savePicture, PICS_FOLDER, getPics, clearFolder, deletePicture, renamePicture }
+export { savePicture, PICS_FOLDER, getPics, clearFolder, deletePicture, renamePicture, prepareFolder }
